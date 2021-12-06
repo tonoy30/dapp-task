68cad96b93a7cabea15532a56b0f7c82e64a607a
tonoy30 <tonoy.sust@gmail.com>
2021-12-06 22:11:58 +0600

    writing ui

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,20 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount;
+
+    struct Task {
+        uint256 id;
+        string description;
+        bool completed;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Block chain");
+    }
+
+    function createTask(string memory _description) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _description, false);
+    }
 }
